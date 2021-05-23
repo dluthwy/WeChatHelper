7c4204b09fbc1bf09ca51406c0a6be318c34923b
hwy <dluthwy@gmail.com>
2021-05-23 11:25:38 +0800

    first commit

---
+++ WeChatHelper/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsInput.Native;
+using WindowsInput;
+using System.ComponentModel;
+using CronExpressionDescriptor;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.ObjectModel;
+using HandyControl.Controls;
+using System.Windows.Controls;
+using Quartz.Impl;
+using Quartz;
+using System.Windows.Media.Imaging;
+
+namespace WeChatHelper
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        private ObservableCollection<SendTask> taskDataList = new ObservableCollection<SendTask>();
+        private SendTask curSendTask = new SendTask("", "", new TaskCron(DateTime.Now));
+        private SystemConfig sysConfig = new SystemConfig(Key.W, ModifierKeys.Shift | ModifierKeys.Alt, "");
+
+        private StdSchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        private IScheduler scheduler;
+
+        private System.Windows.Threading.DispatcherTimer wechatMonitorTimer = new System.Windows.Threading.DispatcherTimer();
+        private System.Windows.Threading.DispatcherTimer timeDisplayTimer = new System.Windows.Threading.DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataBindingInit();
+            ScheduleInit();
+            TimerInit();
+        }
+
+        private void DataBindingInit()
+        {
+            this.dgTaskData.ItemsSource = taskDataList;
+            this.tbxSend2Name.DataContext = curSendTask;
+            this.tbxSendContent.DataContext = curSendTask;
+            this.tbxWechatHoyKey.DataContext = sysConfig;
+            this.tpTaskTime.DataContext = curSendTask.SendCron;
+            this.lbxSendWeeks.ItemsSource = curSendTask.SendCron.SendWeeksList;
+            this.tbxCustomCron.DataContext = curSendTask.SendCron;
+            this.rbtnUseCustomCron.DataContext = curSendTask.SendCron;
+        }
+
+        private async void ScheduleInit()
+        {
+            scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.Start();
+        }
+
+        private void TimerInit()
+        {
+            wechatMonitorTimer.Tick += new EventHandler(WechatMonitorTimerExecute);
+            wechatMonitorTimer.Interval = new TimeSpan(0, 0, 1);
+            wechatMonitorTimer.Start();
+
+            timeDisplayTimer.Tick += new EventHandler(TimeDisplayTimerExecute);
+            timeDisplayTimer.Interval = new TimeSpan(0, 0, 1);
+            timeDisplayTimer.Start();
+        }
+
+        private void TimeDisplayTimerExecute(object sender, EventArgs e)
+        {
+            this.tbCurrentTime.Text = DateTime.Now.ToString("F");
+        }
+
+        private void WechatMonitorTimerExecute(object sender, EventArgs e)
+        {
+            wechatMonitorTimer.Stop(); 
+            if (NativeMethods.IsWeChatRun())
+            {
+                this.imgWeChat.Source = new BitmapImage(new Uri("yes.png", UriKind.Relative));
+            }
+            else
+            {
+                this.imgWeChat.Source = new BitmapImage(new Uri("no.png", UriKind.Relative));
+            }
+            wechatMonitorTimer.Start();
+        }
+
+        private void tbxWechatHoyKey_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Don't let the event pass further
+            // because we don't want standard textbox shortcuts working
+            e.Handled = true;
+
+            // Get modifiers and key data
+            var modifiers = Keyboard.Modifiers;
+            var key = e.Key;
+
+            // When Alt is pressed, SystemKey is used instead
+            if (key == Key.System)
+            {
+                key = e.SystemKey;
+            }
+
+            // Pressing delete, backspace or escape without modifiers clears the current value
+            if (modifiers == ModifierKeys.None &&
+                (key == Key.Delete || key == Key.Back || key == Key.Escape))
+            {
+                return;
+            }
+
+            // If no actual key was pressed - return
+            if (key == Key.LeftCtrl ||
+                key == Key.RightCtrl ||
+                key == Key.LeftAlt ||
+                key == Key.RightAlt ||
+                key == Key.LeftShift ||
+                key == Key.RightShift ||
+                key == Key.LWin ||
+                key == Key.RWin ||
+                key == Key.Clear ||
+                key == Key.OemClear ||
+                key == Key.Apps)
+            {
+                return;
+            }
+
+            // Update the value
+            sysConfig.UpdateHotKey(key, modifiers);
+        }
+
+        private void btnHotKeyTest_Click(object sender, RoutedEventArgs e)
+        {
+            if (!NativeMethods.IsWeChatRun())
+            {
+                HandyControl.Controls.MessageBox.Error("未找到微信进程，请先启动微信", "错误");
+                return;
+            }
+            sysConfig.SimulateKeyPress();
+        }
+
+        private async void btnAddTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(curSendTask.Send2Name) ||
+                String.IsNullOrEmpty(curSendTask.SendContent))
+            {
+                HandyControl.Controls.MessageBox.Error("发送对象或内容为空", "错误");
+                return;
+            }
+            SendTask sendTask = (SendTask)curSendTask.Clone();
+            if (await sendTask.StartSchedule(scheduler, sysConfig))
+            {
+                taskDataList.Add(sendTask);
+            }
+        }
+
+        private async void btnDeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.dgTaskData.SelectedItems.Count > 1)
+            {
+                MessageBoxResult result = HandyControl.Controls.MessageBox.Ask(String.Format("确认删除选中的 {0} 项任务", this.dgTaskData.SelectedItems.Count), "提示");
+                if (result != MessageBoxResult.OK)
+                {
+                    return;
+                }
+            }
+            Collection<SendTask> selectedSendTask = new Collection<SendTask>();
+            foreach (var item in this.dgTaskData.SelectedItems)
+            {
+                int rowIndex = this.dgTaskData.Items.IndexOf(item);
+                selectedSendTask.Add(taskDataList[rowIndex]);
+            }
+            foreach (var task in selectedSendTask)
+            {
+                await task.StopSchedule(scheduler);
+                taskDataList.Remove(task);
+            }
+        }
+
+        private void btnSendTest_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(curSendTask.Send2Name) ||
+                String.IsNullOrEmpty(curSendTask.SendContent))
+            {
+                HandyControl.Controls.MessageBox.Error("发送对象或内容为空", "错误");
+                return;
+            }
+            MessageBoxResult result = HandyControl.Controls.MessageBox.Ask(String.Format("确认发送消息给：{0}", curSendTask.Send2Name), "提示");
+            if (result != MessageBoxResult.OK)
+            {
+                return;
+            }
+            curSendTask.DoSendOperation(sysConfig);
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void NotifyIcon_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+                this.WindowState = WindowState.Normal;
+                this.Activate();
+            }
+        }
+    }
+}
---
+++ WeChatHelper/SendTask.cs
@@ -0,0 +1,208 @@
+﻿using CronExpressionDescriptor;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace WeChatHelper
+{
+    public enum TaskStateEnum
+    {
+        WAITED,
+        EXCUTING,
+        FINISHED,
+        WECHAT_ERROR,
+        SYSTEM_ERROR,
+        INTERNAL_ERROR,
+    };
+
+    public class SendTaskJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                SendTask sendTask = (SendTask)context.JobDetail.JobDataMap["sendTask"];
+                SystemConfig sysConfig = (SystemConfig)context.JobDetail.JobDataMap["sysConfig"];
+                if (sendTask == null || sysConfig == null)
+                {
+                    sendTask.State = TaskStateEnum.INTERNAL_ERROR;
+                    return;
+                }
+                if (NativeMethods.IsWorkstationLocked())
+                {
+                    sendTask.State = TaskStateEnum.SYSTEM_ERROR;
+                    return;
+                }
+                if (!NativeMethods.IsWeChatRun())
+                {
+                    sendTask.State = TaskStateEnum.WECHAT_ERROR;
+                    return;
+                }
+
+                sendTask.DoSendOperation(sysConfig);
+                sendTask.State = TaskStateEnum.FINISHED;
+            });
+        }
+    }
+
+    public class SendTask : INotifyPropertyChanged, ICloneable
+    {
+        static private int _taskCount = 0;
+        private string _send2Name = "";
+        private string _sendContent = "";
+        private TaskStateEnum _taskState;
+        private int _sendIndex = 0;
+
+        public TaskCron SendCron { get; set; }
+        
+        private static Dictionary<TaskStateEnum, string> taskStateMap = new Dictionary<TaskStateEnum, string>
+        {
+            {TaskStateEnum.WAITED, "待执行" },
+            {TaskStateEnum.EXCUTING, "执行中" },
+            {TaskStateEnum.FINISHED, "已完成" },
+            {TaskStateEnum.WECHAT_ERROR, "执行失败：微信未运行" },
+            {TaskStateEnum.SYSTEM_ERROR, "执行失败：系统休眠或者自动锁屏" },
+            {TaskStateEnum.INTERNAL_ERROR, "执行失败：内部错误" },
+        };
+
+        public SendTask(string send2Name, string sendContent, TaskCron sendCron)
+        {
+            this._send2Name = send2Name;
+            this._sendContent = sendContent;
+            this.SendCron = sendCron;
+            this._taskState = TaskStateEnum.WAITED;
+            this._sendIndex = _taskCount++;
+        }
+
+        public int Index {
+            get { return _sendIndex; }
+        }
+
+        public string Send2Name {
+            get { return _send2Name; }
+            set {
+                if (string.IsNullOrEmpty(value) && value == _send2Name)
+                    return;
+
+                _send2Name = value;
+                OnPropertyChanged("Send2Name");
+            }
+        }
+
+        public string SendContent {
+            get { return _sendContent; }
+            set {
+                if (string.IsNullOrEmpty(value) && value == _sendContent)
+                    return;
+
+                _sendContent = value;
+                OnPropertyChanged("SendContent");
+            }
+        }
+
+        public string SendCronDescription {
+            get {
+                return ExpressionDescriptor.GetDescription(this.SendCron.ToString(), new Options()
+                {
+                    Use24HourTimeFormat = true,
+                });
+            }
+        }
+
+        public TaskStateEnum State {
+            get { return _taskState; }
+            set {
+                if (value == _taskState)
+                    return;
+                _taskState = value;
+                OnPropertyChanged("TaskStateDescription");
+            }
+        }
+
+        public string TaskStateDescription {
+            get { return taskStateMap[_taskState]; }
+        }
+
+        public async Task<bool> StartSchedule(IScheduler scheduler, SystemConfig sysConfig)
+        {
+            try
+            {
+                // 创建作业
+                var jobDetail = JobBuilder.Create<SendTaskJob>()
+                                            .WithIdentity("SendTask" + _sendIndex.ToString())
+                                            .SetJobData(new JobDataMap() {
+                                            new KeyValuePair<string, object>("sendTask", this),
+                                            new KeyValuePair<string, object>("sysConfig", sysConfig),
+                                            })
+                                            .Build();
+                // 创建触发器
+                var trigger = TriggerBuilder.Create()
+                                .WithCronSchedule(this.SendCron.ToString())
+                                .Build();
+                // 添加调度
+                await scheduler.ScheduleJob(jobDetail, trigger);
+            } catch (Exception ex)
+            {
+                HandyControl.Controls.MessageBox.Error(ex.Message, "错误");
+                return false;
+            }
+            return true;
+        }
+
+        public async Task StopSchedule(IScheduler scheduler)
+        {
+            JobKey jk = new JobKey("SendTask" + _sendIndex.ToString());
+            if (await scheduler.CheckExists(jk))
+            {
+                await scheduler.DeleteJob(jk);
+            }
+        }
+
+        public void DoSendOperation(SystemConfig sysConfig)
+        {
+            // 打开微信应用
+            sysConfig.SimulateKeyPress();
+            Thread.Sleep(100);
+
+            var simulator = new InputSimulator();
+            // 搜索用户名
+            simulator.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
+            simulator.Keyboard.KeyPress(VirtualKeyCode.VK_F);
+            simulator.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
+            Thread.Sleep(100);
+
+            simulator.Keyboard.TextEntry(_send2Name);
+            Thread.Sleep(1000);
+            simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            Thread.Sleep(800);
+
+            // 发送信息
+            simulator.Keyboard.TextEntry(_sendContent);
+            simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            Thread.Sleep(1000);
+
+            // 关闭应用
+            sysConfig.SimulateKeyPress();
+        }
+
+        public object Clone()
+        {
+            return new SendTask(_send2Name, _sendContent, SendCron);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected internal virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
